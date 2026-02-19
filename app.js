e7632ce3798db74188599d34bd64d371ef135a33
farshid <40495236+farshidweb2012@users.noreply.github.com>
2026-02-19 13:14:27 +0330

    after eddit in 29/bahaman working correctly

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 var foodList = [];
 let Foodlist;
+let cartItem = [];
+
 const cardContainer = document.getElementById("cardContainer");
- 
+const cartCountEL = document.querySelector(".cartNumber");
+const Modal = document.querySelector(".modal");
+const modalCloseBtn = document.querySelector(".modal-exitBtn");
 async function fetchData() {
   try {
     const response = await fetch(
@@ -20,7 +24,7 @@ async function fetchData() {
       return meal;
     });
 
-    renderDataToDom(foodList);
+    renderDataToDom(Foodlist);
   } catch (error) {
     console.log(error);
   }
@@ -64,39 +68,107 @@ function manageAtuh() {
 window.addEventListener("load", () => {
   fetchData();
   manageAtuh();
+  updateCart();
 });
- 
+////add  to cart
+function addToCart(food) {
+  const cart = loadUserCart();
+ cartItem=cart;
+
+  const existinFood = cart.find((item) => item.id === food.idMeal);
+
+  if (existinFood) {
+    existinFood.quantity++;
+    existinFood.price += food.price;
+  } else {
+    cart.push({
+      id: food.idMeal,
+      foodName: food.strMeal,
+      image: food.strMealThumb,
+      price: food.price,
+      quantity: 1,
+    });
+  }
+  console.log(cart);
+  saveUserCart(cart);
+  updateCart()
+}
+////////render data to dom
 function renderDataToDom(list) {
-    console.log(list)
-  list.map((food) => {
+  list.map((food, index) => {
     const card = document.createElement("div");
-    card.id = food.idMeal;
+    const addBtn = document.createElement("button");
+    addBtn.textContent = "add";
+    addBtn.id = food.idMeal;
+    addBtn.classList.add("card-btn");
+    addBtn.onclick = () => addToCart(food);
     card.classList.add("card");
     card.innerHTML = `
-         <img class='foodImage' src='${food.strMealThumb}' />
-         <p>${food.strMeal}</P>
-         <span>${convertNumberToPersian(food.price)}$</span>
-         <button     id=${food.idMeal} id='btn-addCart'  class='card-btn'>اضافه کردن یه سبد خرید</button>
-      `;
-    cardContainer.appendChild(card)
+      <img src='${food.strMealThumb}'   style="width: 100%; height: 100% ;"  />
+      <span> ${convertNumberToPersian(food.price)}<p> : قیمت
+    `;
+    card.appendChild(addBtn);
+    cardContainer.appendChild(card);
   });
 }
- 
- 
+//////handle Click
 
+////////////////create  modal
+document.querySelector("#cartIcon").addEventListener("click", (e) => {
+  Modal.style.display = "block";
+  displayCartModal();
+});
+modalCloseBtn.addEventListener("click", () => {
+  Modal.style.display = "none";
+});
 
+window.addEventListener("click", (e) => {
+  if (e.target.classList.contains("modal")) {
+    Modal.style.display = "none";
+  }
+});
+function displayCartModal() {
+  console.log("display modal ");
+}
+//////////////////////Save User Cart
+function saveUserCart(cart) {
+  const username = localStorage.getItem("loggedUser");
+  localStorage.setItem(`cart_${username}`, JSON.stringify(cart));
+}
+/////////////update cart in dom element
+function updateCart() {
+  const cart = loadUserCart();
+  const countCart = cart.reduce((sum, item) => sum + item.quantity, 0);
+
+  cartCountEL.textContent = countCart;
+}
+
+//////////////////////////Load User Cart
+function loadUserCart() {
+  const loggedUser = localStorage.getItem("loggedUser");
+  if (!loggedUser) {
+    return [];
+  }
+  const getUserCart = localStorage.getItem(`cart_${loggedUser}`);
+  if (!getUserCart) {
+    return [];
+  }
+
+  return JSON.parse(getUserCart);
+}
+//////////////////convert to persian language
 function convertNumberToPersian(number) {
   const numbers = ["۰", "۱", "۲", "۳", "۴", "۵", "۶", "۷", "۸", "۹"];
   return number.toString().replace(/\d/g, (digit) => numbers[digit]);
 }
 
-const allCountryCategory = Array.from(
-  document.querySelectorAll(".countryImage"),
-);
-allCountryCategory.map((country) => {
-  country.addEventListener("click", (e) => {
-    if (country.getAttribute("value") === "All") {
-      country.classList.add("selected-country");
-    }
-  });
-});
+// const allCountryCategory = Array.from(
+//   document.querySelectorAll(".countryImage"),
+// );
+// allCountryCategory.map((country) => {
+//   country.addEventListener("click", (e) => {
+//     if (country.getAttribute("value") === "All") {
+//       country.classList.add("selected-country");
+//     }
+//   });
+// });
